f9e3109c7ff30e690bf2fac876b644b5a5c51f79
puetzp <peter.puetz@itenos.de>
2022-01-05 22:41:58 +0100

    Add some dependencies, begin implementing config parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use log::info;
+use yaml_rust::yaml::Yaml;
+use yaml_rust::YamlLoader;
+
+struct NagiosRange(String);
+
+struct Thresholds {
+    warning: NagiosRange,
+    critical: NagiosRange,
+}
+
+struct Mapping {
+    query: String,
+    thresholds: Thresholds,
+    host: String,
+    service: String,
+}
+
+fn parse_mapping(mapping: (&Yaml, &Yaml)) -> bool {
+    true
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let s = "
+---
+mappings:
+  cpu_idle_percentage:
+    query: 'sum(node_cpu_seconds_total{mode=\"idle\"}) / sum(node_cpu_seconds_total)'
+    thresholds: {}
+    host: 'Kubernetes Test'
+    service: 'CPU idle percentage'
+    interval: '1m'
+";
+    let docs = YamlLoader::load_from_str(s).unwrap();
+    let doc = &docs[0];
+    let mut mappings: Vec<Mapping> = vec![];
+
+    match doc.as_hash().unwrap().get(&Yaml::from_str("mappings")) {
+        Some(conf_mappings) => {
+            let conf_mappings = match conf_mappings.as_hash() {
+                Some(m) => m,
+                None => panic!("'mappings' must be a hash"),
+            };
+
+            for map_item in conf_mappings {
+                let parsed = parse_mapping(map_item);
+                mappings.push(parsed);
+            }
+        }
+        None => panic!("Missing mandatory configuration item"),
+    }
 }
