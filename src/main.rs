ae4c61f5544feac7c10666cf81162a5f195ff23a
Peter Sebastian Puetz <peter.puetz@itenos.de>
2022-01-14 14:26:56 +0100

    Read config from file

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,26 @@ mod types;
 use crate::types::Mapping;
 use anyhow::Context;
 use log::info;
+use std::fs::File;
+use std::io::Read;
+
+const DEFAULT_CONFIG_PATH: &str = "/etc/vec2checkd/config.yaml";
 
 fn main() -> Result<(), anyhow::Error> {
     env_logger::init();
-    let s = "
----
-mappings:
-  cpu_idle_percentage:
-    query: 'sum(node_cpu_seconds_total{mode=\"idle\"}) / sum(node_cpu_seconds_total)'
-    host: 'Kubernetes Test'
-    service: 'CPU idle percentage'
-    interval: '1m'
-";
-    let config = config::parse_yaml(s).with_context(|| "failed to parse configuration")?;
+
+    info!("Parsing configuration from '{}'", DEFAULT_CONFIG_PATH);
+    let mut file = File::open(DEFAULT_CONFIG_PATH).with_context(|| {
+        format!(
+            "failed to read configuration file '{}'",
+            DEFAULT_CONFIG_PATH
+        )
+    })?;
+
+    let mut raw_conf = String::new();
+    file.read_to_string(&mut raw_conf)?;
+
+    let config = config::parse_yaml(&raw_conf).with_context(|| "failed to parse configuration")?;
 
     let mappings: Option<Vec<Mapping>> = config::parse_mappings(&config)
         .with_context(|| "failed configuration to parse mappings from configuration")?;
@@ -26,6 +33,6 @@ mappings:
         info!("No mappings configured. Exiting.");
         std::process::exit(0);
     }
-    println!("{:?}", mappings);
+
     Ok(())
 }
