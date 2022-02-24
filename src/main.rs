f7c7f0b645fc85150f11a8e7cf307bf05d7eb0f0
Peter Sebastian Puetz <peter.puetz@itenos.de>
2022-02-24 15:18:05 +0100

    Modify error handling at the top-level
    ... regarding configuration parsing an API client initialization

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ mod util;
 use crate::icinga::*;
 use crate::types::Mapping;
 use crate::util::*;
-use anyhow::Context;
 use gumdrop::Options;
 use log::{debug, error, info};
 use prometheus_http_query::Client as PromClient;
@@ -17,7 +16,6 @@ use std::io::Read;
 use std::str::FromStr;
 use std::time::Instant;
 
-const DEFAULT_CONFIG_PATH: &str = "/etc/vec2checkd/config.yaml";
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[derive(Debug, Options)]
@@ -32,7 +30,7 @@ struct AppOptions {
         help = "load configuration file from a path other than the default",
         short = "c"
     )]
-    config: Option<String>,
+    config: String,
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -46,45 +44,70 @@ async fn main() -> Result<(), anyhow::Error> {
 
     env_logger::init();
 
-    let config_path = opts.config.unwrap_or_else(|| {
-        info!(
-            "No custom config file path provided, falling back to default location: {}",
-            DEFAULT_CONFIG_PATH
-        );
-        DEFAULT_CONFIG_PATH.to_string()
-    });
+    info!("Starting vec2checkd version {}", &VERSION);
 
     let config = {
-        info!("Parse configuration from '{}'", config_path);
-        let mut file = File::open(&config_path)
-            .with_context(|| format!("failed to read configuration file '{}'", config_path))?;
+        info!("Parse configuration from '{}'", opts.config);
+
+        if opts.config.is_empty() {
+            error!("Path to configuration file cannot be empty. Exiting");
+            std::process::exit(1);
+        }
+
+        let mut file = match File::open(&opts.config) {
+            Ok(f) => f,
+            Err(e) => {
+                error!(
+                    "failed to read configuration file '{}': {:#}",
+                    opts.config, e
+                );
+                std::process::exit(1);
+            }
+        };
 
         let mut raw_conf = String::new();
         file.read_to_string(&mut raw_conf)?;
 
-        config::parse_yaml(&raw_conf).with_context(|| "failed to parse configuration file")?
+        match config::parse_yaml(&raw_conf) {
+            Ok(cfg) => cfg,
+            Err(e) => {
+                error!(
+                    "failed to parse configuration file '{}': {:#}",
+                    opts.config, e
+                );
+                std::process::exit(1);
+            }
+        }
     };
 
     info!("Read mappings between PromQL and Icinga check results from configuration");
-    let mut mappings: Vec<Mapping> = config::parse_mappings(config.clone())
-        .with_context(|| "failed to parse mappings from configuration")?;
+    let mut mappings: Vec<Mapping> = match config::parse_mappings(config.clone()) {
+        Ok(m) => m,
+        Err(e) => {
+            error!("failed to parse mappings from configuration: {:#}", e);
+            std::process::exit(1);
+        }
+    };
 
     if mappings.is_empty() {
-        info!("No mappings configured. Shutdown.");
+        info!("No mappings configured. Exiting.");
         std::process::exit(0);
     }
 
     let prom_client = {
         info!("Read Prometheus section from configuration");
-        let c = config::parse_prom_section(&config)
-            .with_context(|| "failed to parse Prometheus section from configuration")?;
+        let c = match config::parse_prom_section(&config) {
+            Ok(c) => c,
+            Err(e) => {
+                error!("Failed to parse Icinga section from configuration: {:#}", e);
+                std::process::exit(1);
+            }
+        };
         info!("Initialize Prometheus API client");
-        match PromClient::from_str(&c.host)
-            .with_context(|| "failed to initialize Prometheus API client")
-        {
+        match PromClient::from_str(&c.host) {
             Ok(clt) => clt,
             Err(e) => {
-                error!("{:?}", e);
+                error!("Failed to initialize Prometheus API client: {:#}", e);
                 std::process::exit(1)
             }
         }
@@ -92,13 +115,18 @@ async fn main() -> Result<(), anyhow::Error> {
 
     let icinga_client = {
         info!("Read Icinga section from configuration");
-        let c = config::parse_icinga_section(&config)
-            .with_context(|| "failed to parse Icinga section from configuration")?;
+        let c = match config::parse_icinga_section(&config) {
+            Ok(c) => c,
+            Err(e) => {
+                error!("Failed to parse Icinga section from configuration: {:#}", e);
+                std::process::exit(1);
+            }
+        };
         info!("Initialize Icinga API client");
-        match IcingaClient::new(&c).with_context(|| "failed to initialize Icinga API client") {
+        match IcingaClient::new(&c) {
             Ok(clt) => clt,
             Err(e) => {
-                error!("{:?}", e);
+                error!("Failed to initialize Icinga API client: {:#}", e);
                 std::process::exit(1)
             }
         }
